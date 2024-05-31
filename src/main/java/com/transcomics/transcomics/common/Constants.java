8a059aa1d5ff6ef4ac1c20e6bb5b12cbc182ae50
lehongquan <lhquan244@gmail.com>
2024-05-31 16:57:57 +0700

    first commit

---
+++ src/main/java/com/transcomics/transcomics/common/Constants.java
@@ -0,0 +1,10 @@
+package com.transcomics.transcomics.common;
+
+/**
+ * Le-Hong-Quan
+ * Date: 29/05/2024
+ * Time: 11:16
+ */
+public class Constant {
+    public static final String Authorization = "Authorization";
+}
